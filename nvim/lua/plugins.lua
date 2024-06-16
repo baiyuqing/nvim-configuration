aaa5697d4f1dd22ffc7f052527c6305f2a165ebd
baiyuqing <baiyuq@gmail.com>
2024-06-16 12:16:31 +0800

    update Signed-off-by: baiyuqing <baiyuq@gmail.com>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -256,6 +256,7 @@ return require('packer').startup(function(use)
 
     use { "hedyhli/outline.nvim" }
     use {'AlexvZyl/nordic.nvim'}
+    use { "j-hui/fidget.nvim" }
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
